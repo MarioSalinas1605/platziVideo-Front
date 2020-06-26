540dd4a35907a0084aac23f10ae3a1bda74b6a9b
Mario Salinas <mario_1605@live.com.mx>
2020-06-26 15:46:56 -0500

    feat: search bar

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Header from '../components/Header';
 
+import Header from '../components/Header';
+import Search from '../components/Search';
 import '../assets/styles/App.scss';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Search />
     </div>
   );
 }
