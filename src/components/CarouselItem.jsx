0752be9e6dc0fb1b737df79a28932768a3c89318
Mario Salinas <mario_1605@live.com.mx>
2020-06-28 09:59:28 -0500

    feat: verify props of carousel item

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
@@ -22,6 +23,14 @@ function CarouselItem({ cover, title, year, contentRating, duration }) {
       </div>
     </div>
   );
+}
+
+CarouselItem.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number
 }
 
 export default CarouselItem;
