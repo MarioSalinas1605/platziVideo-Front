07a315144bc0cfd5da720f46c9881d0f546c4361
Mario Salinas <mario_1605@live.com.mx>
2020-06-27 22:01:25 -0500

    feat: fetch data and show on UI

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -4,21 +4,21 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-function CarouselItem() {
+function CarouselItem({ cover, title, year, contentRating, duration }) {
   return (
     <div className="carousel-item">
       <img
         className="carousel-item__img"
-        src="https://images.pexels.com/photos/4319664/pexels-photo-4319664.jpeg?cs=srgb&dl=ligero-puesta-de-sol-hombre-gente-4319664.jpg&fm=jpg"
-        alt="People"
+        src={cover}
+        alt={title}
       />
       <div className="carousel-item__details">
         <div>
           <img className="carousel-item__details--img" src={playIcon} alt="Play" />
           <img className="carousel-item__details--img" src={plusIcon} alt="Plus" />
         </div>
-        <p className="carousel-item__details--title">Título descríptivo</p>
-        <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+        <p className="carousel-item__details--title">{title}</p>
+        <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
       </div>
     </div>
   );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -9,32 +9,47 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 function App() {
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
+
+  useEffect(() => {
+    fetch(' http://localhost:3000/initialState')
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Search />
 
-      <Categories title="Mi lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {
+        videos.mylist.length > 0 && (
+        <Categories title="Mi lista">
+          <Carousel>
+            <CarouselItem />
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+        )
+      }
 
       <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {
+            videos.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
         </Carousel>
       </Categories>
 
       <Categories title="Originales">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {
+            videos.originals.map((item) => <CarouselItem key={item.id} {...item} />)
+          }
         </Carousel>
       </Categories>
 
