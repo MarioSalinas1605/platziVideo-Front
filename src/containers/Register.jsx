8e8db373ffbcb6d0b2bfea49ad385db9336a29e7
Mario Salinas <mario_1605@live.com.mx>
2020-07-09 07:57:28 -0500

    feat: handle state of register form

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Register.scss';
 
 function Register() {
+  const [form, setForm] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  function handleInput(event) {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(form);
+  }
+
   return (
     <section className="register">
       <section className="register__container">
         <h2>Regístro</h2>
-        <form className="register__container--form">
-          <input className="input" type="name" placeholder="Nombre" />
-          <input className="input" type="text" placeholder="Correo" />
-          <input className="input" type="password" placeholder="Contraseña" />
-          <button type="button" className="button">Registrame</button>
+        <form className="register__container--form" onSubmit={handleSubmit}>
+          <input
+            name="name"
+            className="input"
+            type="name"
+            placeholder="Nombre"
+            onChange={handleInput}
+          />
+          <input
+            name="email"
+            className="input"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button type="submit" className="button">Registrame</button>
         </form>
         <Link to="/login">
           <button type="button" className="button-transparent">Iniciar sesión</button>
