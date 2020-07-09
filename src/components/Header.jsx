bee3bb3c829a5c5968518b52ff4a66ebd29f4db7
Mario Salinas <mario_1605@live.com.mx>
2020-07-09 06:52:08 -0500

    feat: show avatar from email with gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
 function Header() {
+  const user = useSelector((state) => state.user);
+  const hasUser = Object.keys(user).length > 0;
   return (
     <header className="header">
       <Link to="/">
@@ -13,7 +17,11 @@ function Header() {
       </Link>
       <div className="header__menu">
         <div className="header__menu--profile">
-          <img src={userIcon} alt="User" />
+          {
+            hasUser
+              ? <img src={gravatar(user.email)} alt={user.email} />
+              : <img src={userIcon} alt="User" />
+          }
           <p>Perfil</p>
         </div>
         <ul>
