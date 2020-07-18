a3d0648a86332ce2537471dba91da158198e561b
Mario Salinas <mario_1605@live.com.mx>
2020-07-18 14:07:41 -0500

    feat: hot reload

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,28 @@
+/* eslint-disable global-require */
 import express from 'express';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
+
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 
 const app = express();
+
+if (ENV === 'development') {
+  console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = {
+    port: PORT,
+    hot: true,
+  };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.get('*', (req, res) => {
   res.json({
@@ -8,7 +30,7 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.log(err);
   else console.log('Server running on 3000');
 });
