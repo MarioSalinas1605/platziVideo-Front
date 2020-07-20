32f46843ef471b6c5c08f8e10b2998eac6044479
Mario Salinas <mario_1605@live.com.mx>
2020-07-20 06:42:38 -0500

    feat: create history and abstract routes

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './routes/App';
-import initialState from '../../initialStateRedux';
+import initialState from './initialState';
 import reducer from './reducers';
 
+const history = createBrowserHistory();
 const store = createStore(reducer, initialState, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app'),
 );
